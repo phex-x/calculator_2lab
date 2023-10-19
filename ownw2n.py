3f719722bd343a9674c00d712cf159482006f9ef
gogingrisha@gmail.com <gogingrisha@gmail.com>
2023-10-19 14:51:24 +0300

    Changes to be committed: 	new file:   calca.py 	new file:   ch.py 	new file:   ownw2n.py 	new file:   wtonum.py

---
+++ ownw2n.py
@@ -0,0 +1,169 @@
+number_system = {
+    'ноль': 0,
+    'один': 1,
+    'два': 2,
+    'три': 3,
+    'четыре': 4,
+    'пять': 5,
+    'шесть': 6,
+    'семь': 7,
+    'восемь': 8,
+    'девять': 9,
+    'десять': 10,
+    'одиннадцать': 11,
+    'двенадцать': 12,
+    'тринадцать': 13,
+    'четырнадцать': 14,
+    'пятнадцать': 15,
+    'шестнадцать': 16,
+    'семнадцать': 17,
+    'восемнадцать': 18,
+    'девятнадцать': 19,
+    'двадцать': 20,
+    'тридцать': 30,
+    'сорок': 40,
+    'пятьдесят': 50,
+    'шестьдесят': 60,
+    'семьдесят': 70,
+    'восемьдесят': 80,
+    'девяносто': 90,
+    'сто': 100,
+}
+
+decimal_words = ['ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять']
+
+def number_formation(number_words):
+    numbers = []
+    for number_word in number_words:
+        numbers.append(number_system[number_word])
+    if len(numbers) == 4:
+        return (numbers[0] * numbers[1]) + numbers[2] + numbers[3]
+    elif len(numbers) == 3:
+        return numbers[0] * numbers[1] + numbers[2]
+    elif len(numbers) == 2:
+        if 100 in numbers:
+            return numbers[0] * numbers[1]
+        else:
+            return numbers[0] + numbers[1]
+    else:
+        return numbers[0]
+
+def get_decimal_sum(decimal_digit_words):
+    decimal_number_str = []
+    for dec_word in decimal_digit_words:
+        if(dec_word not in decimal_words):
+            return 0
+        else:
+            decimal_number_str.append(number_system[dec_word])
+    final_decimal_string = '0.' + ''.join(map(str,decimal_number_str))
+    return float(final_decimal_string)
+
+def word_to_num(number_sentence):
+    if type(number_sentence) is not str:
+        raise ValueError("Type of input is not string! Please enter a valid number word (eg. \'two million twenty three thousand and forty nine\')")
+
+    number_sentence = number_sentence.replace('-', ' ')
+    number_sentence = number_sentence.lower()  # converting input to lowercase
+
+    if(number_sentence.isdigit()):  # return the number if user enters a number string
+        return int(number_sentence)
+
+    split_words = number_sentence.strip().split()  # strip extra spaces and split sentence into words
+
+    clean_numbers = []
+    clean_decimal_numbers = []
+
+    # removing and, & etc.
+    for word in split_words:
+        if word in number_system:
+            clean_numbers.append(word)
+
+    # Error message if the user enters invalid input!
+    if len(clean_numbers) == 0:
+        raise ValueError("No valid number words found! Please enter a valid number word (eg. two million twenty three thousand and forty nine)") 
+
+    # Error if user enters million,billion, thousand or decimal point twice
+    if clean_numbers.count('thousand') > 1 or clean_numbers.count('million') > 1 or clean_numbers.count('billion') > 1 or clean_numbers.count('point')> 1:
+        raise ValueError("Redundant number word! Please enter a valid number word (eg. two million twenty three thousand and forty nine)")
+
+    # separate decimal part of number (if exists)
+    if clean_numbers.count('point') == 1:
+        clean_decimal_numbers = clean_numbers[clean_numbers.index('point')+1:]
+        clean_numbers = clean_numbers[:clean_numbers.index('point')]
+
+    billion_index = clean_numbers.index('billion') if 'billion' in clean_numbers else -1
+    million_index = clean_numbers.index('million') if 'million' in clean_numbers else -1
+    thousand_index = clean_numbers.index('thousand') if 'thousand' in clean_numbers else -1
+
+    if (thousand_index > -1 and (thousand_index < million_index or thousand_index < billion_index)) or (million_index>-1 and million_index < billion_index):
+        raise ValueError("Malformed number! Please enter a valid number word (eg. two million twenty three thousand and forty nine)")
+
+    total_sum = 0  # storing the number to be returned
+
+    if len(clean_numbers) > 0:
+        # hack for now, better way TODO
+        if len(clean_numbers) == 1:
+                total_sum += number_system[clean_numbers[0]]
+
+        else:
+            if billion_index > -1:
+                billion_multiplier = number_formation(clean_numbers[0:billion_index])
+                total_sum += billion_multiplier * 1000000000
+
+            if million_index > -1:
+                if billion_index > -1:
+                    million_multiplier = number_formation(clean_numbers[billion_index+1:million_index])
+                else:
+                    million_multiplier = number_formation(clean_numbers[0:million_index])
+                total_sum += million_multiplier * 1000000
+
+            if thousand_index > -1:
+                if million_index > -1:
+                    thousand_multiplier = number_formation(clean_numbers[million_index+1:thousand_index])
+                elif billion_index > -1 and million_index == -1:
+                    thousand_multiplier = number_formation(clean_numbers[billion_index+1:thousand_index])
+                else:
+                    thousand_multiplier = number_formation(clean_numbers[0:thousand_index])
+                total_sum += thousand_multiplier * 1000
+
+            if thousand_index > -1 and thousand_index != len(clean_numbers)-1:
+                hundreds = number_formation(clean_numbers[thousand_index+1:])
+            elif million_index > -1 and million_index != len(clean_numbers)-1:
+                hundreds = number_formation(clean_numbers[million_index+1:])
+            elif billion_index > -1 and billion_index != len(clean_numbers)-1:
+                hundreds = number_formation(clean_numbers[billion_index+1:])
+            elif thousand_index == -1 and million_index == -1 and billion_index == -1:
+                hundreds = number_formation(clean_numbers)
+            else:
+                hundreds = 0
+            total_sum += hundreds
+    # adding decimal part to total_sum (if exists)
+    if len(clean_decimal_numbers) > 0:
+        decimal_sum = get_decimal_sum(clean_decimal_numbers)
+        total_sum += decimal_sum
+
+    return total_sum
+
+s=input().split()
+for i in range(len(s)):
+    if s[i]=='плюс':
+        s[i]='+'
+    elif s[i]=='минус':
+        s[i]='-'
+    elif s[i]=='умножить' or s[i]=='на':
+        s[i]='*'
+    elif s[i]=='поделить':
+        s[i]='/'
+    else:
+        s[i]=(word_to_num(s[i]))
+
+for i in range(len(s)-1):
+    if s[i]=='*' and s[i+1]=='*':
+        del s[i]
+    if s[i]=='/' and s[i+1]=='/':
+        del s[i]
+print(s)
+new_s=''
+for i in range(len(s)):
+    new_s+=str(s[i])
+print(eval(new_s))
---
+++ wtonum.py
@@ -0,0 +1,4 @@
+from word2number import w2n
+
+s=input()
+print(w2n.word_to_num(s))
